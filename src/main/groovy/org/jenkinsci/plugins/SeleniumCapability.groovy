a8b7b5e118cd67af5e621ff020ba5a611c1fc867
JeremyMarshall <jeremystuartmarshall@gmail.com>
2013-09-13 08:52:32 +1000

    missed a file

---
+++ src/main/groovy/org/jenkinsci/plugins/SeleniumCapability.groovy
@@ -0,0 +1,76 @@
+package org.jenkinsci.plugins
+
+import hudson.Extension
+//import hudson.model.Descriptor
+import org.kohsuke.stapler.DataBoundConstructor
+
+import java.util.regex.Matcher
+
+class SeleniumCapability extends  ComplexAxisItem implements Comparable {
+
+    private Integer maxInstances
+    private String browserName
+    private String platformName
+    private String browserVersion
+
+    SeleniumCapability() {
+        browserName = "Any"
+        platformName = "Any"
+        browserVersion = "Any"
+        maxInstances = 1
+    }
+
+    @DataBoundConstructor
+    SeleniumCapability(String browserName, String platformName, String browserVersion) {
+        this.browserName = browserName
+        this.platformName = platformName
+        this.browserVersion = browserVersion
+    }
+
+    SeleniumCapability(String titleAttr) {
+        this()
+
+        Matcher m = (titleAttr =~ /(platform|browserName|version)=(\w+)/)
+
+        while (m.find()) {
+            if (m.group(1).equals("platform"))
+                this.platformName = m.group(2)
+            else if (m.group(1).equals("browserName"))
+                this.browserName = m.group(2)
+            else if (m.group(1).equals("version"))
+                this.browserVersion = m.group(2)
+        }
+    }
+
+    public Integer incr() {
+        maxInstances++
+    }
+
+    public String combinationFilter() {
+        String.format("(TEST_PLATFORM=='%s' && TEST_BROWSER=='%s' && TEST_VERSION=='%s')", platformName, browserName, browserVersion)
+    }
+
+    @Override
+    String toString() {
+        String.format("%s-%s-%s", platformName, browserName, browserVersion)
+    }
+
+
+    public String getBrowserName(){
+        this.browserName
+    }
+
+    public String getPlatformName(){
+        this.platformName
+    }
+
+    public String getBrowserVersion(){
+        this.browserVersion
+    }
+
+    @Extension public static class DescriptorImpl extends ComplexAxisItemDescriptor {
+        @Override public String getDisplayName() {
+            return "Selenium Capability";
+        }
+    }
+}
