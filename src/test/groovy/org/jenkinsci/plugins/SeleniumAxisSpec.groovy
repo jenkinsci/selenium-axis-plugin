080294afa186a9ff47760f81b3ede47b5e1b5282
JeremyMarshall <jeremystuartmarshall@gmail.com>
2013-10-18 13:16:57 +1100

    jacoco support (not very groovy) and fixed spock tests in cosures

--- src/test/groovy/org/jenkinsci/plugins/SeleniumAxisSpec.groovy
+++ src/test/groovy/org/jenkinsci/plugins/SeleniumAxisSpec.groovy
@@ -10,44 +10,46 @@ class SeleniumAxisSpec extends Specification {
     @Rule
     JenkinsRule rule = new JenkinsRule()
 
-    //def sel = new Selenium(Selenium.load("/grid-2.35.0.html"), SeleniumCapabilityRO.class)
-    //def selCap = new SeleniumDynamicCapability(sel.getSeleniumCapabilities())
-    //def sdca = new ArrayList<ComplexAxisItem>()
+    def sel = new Selenium(Selenium.load("/grid-2.35.0.html"), SeleniumCapabilityRO.class)
+    def selCap = new SeleniumDynamicCapability(sel.getSeleniumCapabilities())
+    def sdca = new ArrayList<ComplexAxisItem>()
 
-    //def 'Dynamic Add'() {
-    //    given:
+    def 'Dynamic Add'() {
+        given:
 
-     //   def matrixProject = rule.createMatrixProject()
-     //   sdca.add(selCap)
-     //   SeleniumDynamicCapability.DescriptorImpl.getTopLevelDescriptor().setServer("/grid-2.35.0.html")
+        def matrixProject = rule.createMatrixProject()
+        sdca.add(selCap)
+        SeleniumDynamicCapability.DescriptorImpl.getTopLevelDescriptor().setServer("/grid-2.35.0.html")
 
-     //   def axis = new SeleniumAxis('TEST', sdca)
-     //   matrixProject.axes.add(axis)
+        def axis = new SeleniumAxis('TEST', sdca)
+        matrixProject.axes.add(axis)
 
-        //when:
-        //def build = matrixProject.scheduleBuild2(0).get()
-        //then:
-        //build.logFile.text.contains("Hello, kiy0taka!")
+        when:
+        def build = matrixProject.scheduleBuild2(0).get()
+        then:
+        build.logFile.text.contains("Hello, kiy0taka!")
 
-     //   when:
-     //   def runs = matrixProject.scheduleBuild2(0).get().getRuns()
-     //   then:
-     //   runs.each(){it.logFile.text.contains("Hello, kiy0taka!")}
-     //
-    //}
+        when:
+        def runs = matrixProject.scheduleBuild2(0).get().getRuns()
+        then:
+        runs.each(){it.logFile.text.contains("Hello, kiy0taka!")}
+     
+    }
 
-    //def 'matrix'() {
-    //    given:
-    //    def matrixProject = rule.createMatrixProject()
-    //    def axis = new TextAxis('TEST', "1", "2", "3")
-    //    matrixProject.axes.add(axis)
-    //
-        //expect: matrixProject.scheduleBuild2(0).get().logFile.text.contains("Some String!")
+    def 'matrix'() {
+        given:
+        def matrixProject = rule.createMatrixProject()
+        def axis = new TextAxis('TEST', "1", "2", "3")
+        matrixProject.axes.add(axis)
+    
+        def b =  matrixProject.scheduleBuild2(0).get()
 
-    //    matrixProject.scheduleBuild2(0).get().getRuns().every{
-    //        expect: it.logFile.text.contains("Another String")
-    //    }
-    //}
+        expect: b.logFile.text.contains("SUCCESS")
+
+        b.getRuns().each(){
+            assert it.logFile.text.contains("TEST/1")
+        }
+    }
 
     def 'matrix2'() {
         given:
@@ -56,11 +58,40 @@ class SeleniumAxisSpec extends Specification {
         matrixProject.axes.add(axis)
 
         expect:
-        matrixProject.scheduleBuild2(0).get().getRuns().every{
+        matrixProject.scheduleBuild2(0).get().getRuns().each(){
             //logFile.text.contains("Some String!")
             //runs.every { it.logFile.text.contains("Another String") }
-            it.logFile.text.contains("Another String")
+            assert it.logFile.text.contains("TEST/1")
         }
     }
+
+    def 'matrix3'() {
+        given:
+        def matrixProject = rule.createMatrixProject()
+        def axis = new TextAxis('TEST', "1", "2", "3")
+        matrixProject.axes.add(axis)
+
+        expect:
+        matrixProject.scheduleBuild2(0).get().with(){
+            logFile.text.contains("Some String!")
+            getRuns().each() { assert it.logFile.text.contains("TEST/1") }
+        }
+    }
+
+    def 'matrix4'() {
+        given:
+        def matrixProject = rule.createMatrixProject()
+        def axis = new TextAxis('TEST', "1", "2", "3")
+        matrixProject.axes.add(axis)
+
+        expect:
+        with({ matrixProject.scheduleBuild2(0).get()}){
+            logFile.text.contains("Some String!")
+            it.getRuns().each() { assert it.logFile.text.contains("TEST/1") }
+        }
+    }
+
+
 }
+
 
