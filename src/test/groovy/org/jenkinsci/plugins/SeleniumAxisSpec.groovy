a95f260a6404e6b486736c7f89ae9599a7442f14
JeremyMarshall <jeremystuartmarshall@gmail.com>
2013-10-23 10:57:57 +1100

    added manual selenium test

--- src/test/groovy/org/jenkinsci/plugins/SeleniumAxisSpec.groovy
+++ src/test/groovy/org/jenkinsci/plugins/SeleniumAxisSpec.groovy
@@ -18,12 +18,13 @@ class SeleniumAxisSpec extends Specification {
         if(seleniumAxisDescriptor == null){
             seleniumAxisDescriptor= Jenkins.getInstance().getDescriptor(SeleniumAxis.class)
         }
+
+        seleniumAxisDescriptor.setServer(seleniumFile)
+
         def matrixProject = rule.createMatrixProject()
 
         def axis = new SeleniumAxis('TEST', seleniumAxisDescriptor.loadDefaultItems())
         def axl = new AxisList();
-
-        seleniumAxisDescriptor.setServer(seleniumFile)
 
         axl.add(axis)
         matrixProject.setAxes(axl)
@@ -31,6 +32,25 @@ class SeleniumAxisSpec extends Specification {
         matrixProject
     }
 
+    MatrixProject configureManual(){
+
+        def matrixProject = rule.createMatrixProject()
+
+        def sc = new SeleniumCapability();
+        def sc2 = new SeleniumCapability('Browser', 'Platform', 'Version')
+        def scont = new ArrayList<SeleniumCapability>()
+        scont.add(sc)
+        scont.add(sc2)
+
+        def axis = new SeleniumAxis('TEST', scont)
+
+        def axl = new AxisList();
+
+        axl.add(axis)
+        matrixProject.setAxes(axl)
+
+        matrixProject
+    }
 
     def 'Dynamic'() {
         given:
@@ -44,29 +64,20 @@ class SeleniumAxisSpec extends Specification {
         build.logFile.text.contains("SUCCESS")
         runs.every(){it.logFile.text.contains("SUCCESS")}
         runs.size() == 4
-
     }
 
     def 'Dynamic Grid Gone'() {
         given:
-        def sad = Jenkins.getInstance().getDescriptor(SeleniumAxis.class)
+        def matrixProject = configure("/grid-2.35.0.html")
 
-        def matrixProject = rule.createMatrixProject()
-        AxisList axl = new AxisList();
-
-        sad.setServer("/grid-2.35.0.html")
-
-        def axis = new SeleniumAxis('TEST', sad.loadDefaultItems())
-        axl.add(axis)
-        matrixProject.setAxes(axl)
-
-        sad.setServer("null://")
+        when:
+        seleniumAxisDescriptor.setServer("null://")
         def build = matrixProject.scheduleBuild2(0).get()
+        def runs = build.getRuns()
 
-        expect:
+        then:
         build.logFile.text.contains("SeleniumException")
-        build.getRuns().size() == 0
-
+        runs.size() == 0
     }
 
     def 'Dynamic Grid Arrived'() {
@@ -92,12 +103,10 @@ class SeleniumAxisSpec extends Specification {
         def runs = build.getRuns()
         then:
         runs.each(){assert it.logFile.text.contains("SUCCESS")}
-
     }
 
     def 'Empty Axis'() {
         given:
-
         def matrixProject = rule.createMatrixProject()
 
         AxisList axl = new AxisList();
@@ -110,6 +119,21 @@ class SeleniumAxisSpec extends Specification {
         expect:
         build.logFile.text.contains("SUCCESS")
         build.getRuns().size()==0
+    }
+
+    def 'Manual'() {
+        given:
+        def matrixProject = configureManual()
+
+        when:
+        def build = matrixProject.scheduleBuild2(0).get()
+        def runs = build.getRuns()
+
+        then:
+        build.logFile.text.contains("SUCCESS")
+        runs.every(){it.logFile.text.contains("SUCCESS")}
+        runs.size() == 2
+
     }
 }
 
